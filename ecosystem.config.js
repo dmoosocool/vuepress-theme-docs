52622a2b9b770dec2b2cca218d111cf22126d7b8
郭鑫 <guoxin@guoxindeMacBook-Pro.local>
2019-08-11 15:23:04 +0800

    update ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  apps : [{
+    name: 'docs',
+    script: 'npm',
+    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+    args: 'run build',
+    env: {
+      COMMON_VARIABLE: "true"
+    },
+    env_production: {
+      NODE_ENV: "production"
+    },
+    autorestart: true,
+    watch: true,
+  }],
+
+  deploy : {
+    production : {
+      user : 'admin',
+      host : '202.79.162.177',
+      ref  : 'origin/master',
+      repo : 'git@github.com:dmoosocool/vuepress-theme-docs.git',
+      path : '/home/admin/www/docs',
+      'post-deploy' : 'npm install && pm2 startOrRelstart ecosystem.config.js --env production'
+    }
+  }
+};
